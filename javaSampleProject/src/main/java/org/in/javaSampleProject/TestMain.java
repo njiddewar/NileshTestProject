3fe9ac74d7275e21da1c6ed10932aad6821f1a21
Nilesh Jiddewar <nileshjiddewar@yahoo.co.in>
2019-05-07 18:07:24 +0530

    Nilesh Jiddewar
    Signed-off-by: Nilesh Jiddewar <nileshjiddewar@yahoo.co.in>

--- javaSampleProject/src/main/java/org/in/javaSampleProject/TestMain.java
+++ javaSampleProject/src/main/java/org/in/javaSampleProject/TestMain.java
@@ -3,8 +3,9 @@ package org.in.javaSampleProject;
 public class TestMain {
 	
 	public static void main(String[] args) {
-		System.out.println("Nilesh Jiddewar");
-		System.out.println("Anish Vasavan");
+		System.out.println("Kattu");
+		System.out.println("Anna");
+		
 	}
 
 }
