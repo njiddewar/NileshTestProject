356ef3516e0e8824c17a069ca518f6fc9c3aaa1a
Nilesh Jiddewar <41741833+njiddewar@users.noreply.github.com>
2019-05-07 18:35:47 +0530

    Updated with some minor changes
    Updated with Minor

--- javaSampleProject/src/main/java/org/in/javaSampleProject/TestMain.java
+++ javaSampleProject/src/main/java/org/in/javaSampleProject/TestMain.java
@@ -5,7 +5,6 @@ public class TestMain {
 	public static void main(String[] args) {
 		System.out.println("Nilesh Jiddewar");
 		System.out.println("Anish Vasavan");
-		System.out.println("Kattu");
 		System.out.println("Harshal Pande");
 	}
 
